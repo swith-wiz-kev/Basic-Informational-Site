301cd66e3cfa568cfd37b9f5345cde2cccee9ed8
kev <ddrain10@gmail.com>
2023-09-07 18:12:10 +0800

    Add correct server response

--- index.js
+++ index.js
@@ -7,30 +7,36 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(
-    (() => {
-      try {
-        const data = fs.readFileSync("index.html", "utf8");
-        return data;
-      } catch (err) {
-        return "error";
-      }
-    })()
-    /* 
-    fs.readFileSync("index.html", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
-        return data;
-      }
-    }) */
-  );
+  const requestedFileName = (() => {
+    switch (req.url) {
+      case "/about":
+        return "about.html";
+      case "/contact-me":
+        return "contact-me.html";
+      case "/":
+        return "index.html";
+      default:
+        return "404.html";
+    }
+  })();
+  if (req.rawHeaders.includes("Referer")) {
+    res.statusCode = 200;
+    res.end();
+  } else {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      (() => {
+        try {
+          const data = fs.readFileSync(requestedFileName, "utf8");
+          return data;
+        } catch (err) {
+          return "error";
+        }
+      })()
+    );
+  }
 });
-
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
